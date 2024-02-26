217b5da6a50e273d422f244d30070751ecf3ff98
Santiago Delgado <sdelgadoc@users.noreply.github.com>
2024-02-26 13:51:35 +0000

    Added multiple files

---
+++ admingpt_cli.py
@@ -0,0 +1,36 @@
+import os, time
+from utils import create_client, run_prompt, poll_for_response
+
+## Assign environmental files
+# Set your OpenAI API key
+os.environ["OPENAI_API_KEY"] = "YOUR OPENAI KEY"
+# Set your Microsoft Graph client ID
+os.environ["CLIENT_ID"] = "YOUR CLIENT ID"
+# Set your Microsoft Graph client secret
+os.environ["CLIENT_SECRET"] = "YOUR CLIENT SECRET"
+
+# Assign constants
+coaching_loop = True
+debug = False
+model = "gpt-4-1106-preview"
+LOOP_DELAY_SECONDS = 3
+
+# Main loop for the application
+while(True):
+    # If it's the first loop, coach the Assistant, otherwise request prompt
+    if coaching_loop:
+        coaching_loop = False
+        # Open the file and read its contents
+        with open("coaching_data.txt", "r") as file:
+            prompt = file = file.read()
+    else:
+        prompt = input("Enter your request here: ")
+        if prompt.lower() == "stop":
+            break
+
+    (client,assistant,thread) = create_client(debug, model)                
+    run = run_prompt(prompt,client,assistant,thread)
+    response = poll_for_response(client, thread, run, model, debug)
+    print(response)
+
+    time.sleep(LOOP_DELAY_SECONDS)
---
+++ utils.py
@@ -0,0 +1,149 @@
+import os, pprint, json, time
+from datetime import datetime as dt
+from openai import OpenAI
+from tools.o365_toolkit import (
+    o365search_emails,
+    o365search_email,
+    o365search_events,
+    o365parse_proposed_times,
+    o365send_message,
+    o365reply_message,
+    o365send_event,
+    o365find_free_time_slots,
+    tools,
+)
+from tools.utils import authenticate
+from globals import st
+
+
+def create_client(debug=False, model=None):
+    # Retrieve user information
+    account = authenticate()
+    mailbox = account.mailbox()
+    mailboxsettings = mailbox.get_settings()
+    timezone = mailboxsettings.timezone
+    directory = account.directory(resource="me")
+    user = directory.get_current_user()
+    client_name = user.full_name
+    client_email = user.mail
+
+    # Set values for global variables
+    assistant_name = "Monica A. Ingenio"
+    current_date = dt.now()
+    formatted_date = current_date.strftime("%A, %B %d, %Y")
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+    assistant_instructions = (
+        "You are an AI Administrative Assistant called "
+        + assistant_name
+        + ", and I am your executive. My name is "
+        + client_name
+        + ". My email is "
+        + client_email
+        + ", and I am in the "
+        + timezone
+        + " timezone. You have access to my email and calendar. Today is "
+        + formatted_date
+        + ". ALWAYS use functions for determining free times and parsing proposed"
+        " meeting times in emails."
+    )
+
+    # Add the debug prompt if user runs with debug
+    if debug:
+        debug_prompt = (
+            "Keep a record of any feedback requests provided by me"
+            " detailing the prompt and tools calls in case I want to retrieve"
+            " them."
+        )
+    else:
+        debug_prompt = ""
+    assistant_instructions += debug_prompt
+
+    client = OpenAI(
+        api_key=openai_api_key,
+    )
+
+    assistant = client.beta.assistants.create(
+        name="AI Administrative Assistant",
+        instructions=assistant_instructions,
+        model=model,
+        tools=tools,
+    )
+
+    thread = client.beta.threads.create()
+
+    return client, assistant, thread
+
+
+def run_prompt(prompt, client, assistant, thread):
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=prompt,
+    )
+
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+    )
+    return run
+
+
+def poll_for_response(client, thread, run, model, debug=False):
+    LOOP_DELAY_SECONDS = 3
+
+    while True:
+        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+        status = run.status
+
+        if status == "completed":
+            response = client.beta.threads.messages.list(thread_id=thread.id)
+            if response.data:
+                return response.data[0].content[0].text.value
+            break
+        elif status == "requires_action":
+            tools_outputs = []
+
+            for tool_call in run.required_action.submit_tool_outputs.tool_calls:
+                tool_call_id = tool_call.id
+                function_name = tool_call.function.name
+                function_arguments = tool_call.function.arguments
+                function_arguments = json.loads(function_arguments)
+
+                # Case statement to execute each toolkit function
+                if function_name == "o365search_emails":
+                    output = o365search_emails(**function_arguments)
+                elif function_name == "o365search_email":
+                    output = o365search_email(**function_arguments)
+                elif function_name == "o365search_events":
+                    output = o365search_events(**function_arguments)
+                elif function_name == "o365parse_proposed_times":
+                    output = o365parse_proposed_times(
+                        **function_arguments, client=client, model=model
+                    )
+                elif function_name == "o365send_message":
+                    output = o365send_message(**function_arguments)
+                elif function_name == "o365send_event":
+                    output = o365send_event(**function_arguments)
+                elif function_name == "o365reply_message":
+                    output = o365reply_message(**function_arguments)
+                elif function_name == "o365find_free_time_slots":
+                    output = o365find_free_time_slots(**function_arguments)
+
+                # Clean the function output into JSON-like output
+                output = pprint.pformat(output)
+                tool_output = {"tool_call_id": tool_call_id, "output": output}
+                tools_outputs.append(tool_output)
+
+            if run.required_action.type == "submit_tool_outputs":
+                client.beta.threads.runs.submit_tool_outputs(
+                    thread_id=thread.id, run_id=run.id, tool_outputs=tools_outputs
+                )
+
+        elif status == "failed":
+            return "Run failed try again!"
+            break
+
+        if debug:
+            print("The Assistant's Status is: " + status)
+
+        time.sleep(LOOP_DELAY_SECONDS)
