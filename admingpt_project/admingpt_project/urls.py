7be4b90dec20415c070681a394dcd83fcec6780a
Santiago Delgado <santiagodelgado@gmail.com>
2024-12-08 15:06:17 -0500

    Commented out admin URL

--- admingpt_project/admingpt_project/urls.py
+++ admingpt_project/admingpt_project/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("", include("email_service.urls")),
 ]
